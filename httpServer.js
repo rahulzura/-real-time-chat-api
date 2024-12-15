f0a28a80ddf0010899882d43737c0160c3dcc5eb
Rahul Kumar <rahulkay3@gmail.com>
2024-12-15 22:16:09 +0530

    video-call

--- httpServer.js
+++ httpServer.js
@@ -1,16 +1,45 @@
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
+import { Server } from 'socket.io';
 import { createServer } from 'node:http';
 import { router } from './routes/index.js';
 
-export const createHttpServer = () => {
-  const app = express();
-  
-  app.use(cors());
-  app.use(bodyParser.json());
-  app.use(router);
+const app = express();
 
-  const server = createServer(app);
-  return server;
+app.use(cors());
+app.use(bodyParser.json());
+app.use(router);
+
+export const server = createServer(app);
+export const io = new Server(server, {
+  cors: {
+    origin: process.env.WS_ALLOWED_ORIGIN,
+    methods: ['GET'],
+  }
+});
+
+import { verifyToken } from './utils/token.js';
+const onConnection = async (socket) => {
+  try {
+    const { token } = socket.handshake.auth;
+    const { id } = await verifyToken(token);
+    await socket.join(id);
+  } catch (err) {
+    console.error('something went wrong while connecting to ws server', err);
+    socket.disconnect();
+  }
+
+  socket.on('message', (msg) => {
+    console.log({ msg });
+    // if (msg?.to && msg.id && msg.text) {
+    //   socket.to(msg.to).emit('message', msg);
+    // } else {
+    //   console.log('invalid message received:', msg);
+    // }
+    io.to('6728aefbd33ef2554e8f947e').except(socket.id).emit('message', msg);
+  });
 };
+
+
+io.on('connection', onConnection);
--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import { connectDb, disconnectDb } from './db/mongo.js';
-import { createHttpServer } from './httpServer.js';
-import { createWsServer } from './wsServer.js';
+import { server } from './httpServer.js';
 
 const { SERVER_PORT } = process.env;
 const serverPort = +SERVER_PORT;
 
 const initService = async () => {
   try {
-    const server = createHttpServer();
-    const io = createWsServer(server);
-
     await connectDb();
     
     server.listen(serverPort, () => {
